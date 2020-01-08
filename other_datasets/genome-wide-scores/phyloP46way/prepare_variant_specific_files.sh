b42671e4bf9d619a5edc0eda5c91ce75d0babf73
kclc950 <dvitsios@gmail.com>
2020-01-08 16:43:03 +0000

    update variant-specific preparation scripts to include ncER pathogenic sets

--- other_datasets/genome-wide-scores/phyloP46way/prepare_variant_specific_files.sh
+++ other_datasets/genome-wide-scores/phyloP46way/prepare_variant_specific_files.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH -t 48:0:0
-#SBATCH -n 4
+#SBATCH -n 1
 #SBATCH --mem=4G
 
-declare -a pathogenic_resources=(clinvar hgmd)
-declare -a benign_resources=(clinvar denovodb denovodb_nonSSC topmed_uniq)
+declare -a pathogenic_resources=(ncER-training ncER-gwas_catalog ncER-test ncER-mendelian ncER-generalization_other ncER-generalization_ncRNA)
+#declare -a pathogenic_resources=(clinvar hgmd)
+#declare -a benign_resources=(clinvar denovodb denovodb_nonSSC topmed_uniq)
 
 full_score_ref_file="All_chromosomes.phyloP46way.bed.bgz"
 score="phyloP46way"
@@ -12,13 +13,13 @@ score="phyloP46way"
 
 # > Pathogenic variant-specific files
 for resource in "${pathogenic_resources[@]}"; do
-	tabix $full_score_ref_file -B ../../variant_annotation/${resource}/${resource}.pathogenic.bed > ${score}.${resource}_pathogenic.bed &
+	tabix $full_score_ref_file -B ../../variant_annotation-hg19/${resource}/${resource}.pathogenic.bed > ${score}.${resource}_pathogenic.bed &
 done
 
 
 # > Benign variant-specific files
 for resource in "${benign_resources[@]}"; do
-	tabix $full_score_ref_file -B ../../variant_annotation/${resource}/${resource}.benign.bed > ${score}.${resource}_benign.bed &
+	tabix $full_score_ref_file -B ../../variant_annotation-hg19/${resource}/${resource}.benign.bed > ${score}.${resource}_benign.bed &
 done
 
 
